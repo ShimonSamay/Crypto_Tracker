65b265370080454fe668a93cdb755ef53dbd7d4e
ShimonSamay <shimonsamay7@gmail.com>
2022-09-20 17:19:34 +0300

    .

--- server/server.js
+++ server/server.js
@@ -21,8 +21,14 @@ app.listen(PORT , () => {
 })
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-    });
+    // app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static('client/build'));
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+    // app.get('*', (req, res) => {
+    //     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    // });
 };
+
+// app.use(express.static('client/build'));
+
+app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
